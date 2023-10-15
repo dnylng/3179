67fd724749c33d06e73835c348c6f405ac51c1b6
Danny Leung <leung.danny45@gmail.com>
2023-10-16 01:49:12 +1100

    assignment2

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,13 +1,15 @@
 const dataUrl = "https://raw.githubusercontent.com/dnylng/3179/main/Assignment2/data/world-happiness-report-2023.csv";
+
 const baseSpec = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
     "data": { "url": dataUrl },
-    "width": 400,
-    "height": 380,
+    "width": 390,
+    "height": 450,
     "mark": {
         "type": "point",
-        "shape": "circle",  
-        "size": 5 
+        "shape": "circle",
+        "filled": true,
+        "size": 5
     },
     "encoding": {
         "x": {
@@ -17,8 +19,40 @@ const baseSpec = {
             "scale": {
                 "domain": [2, 8]
             }
+        },
+        "color": {
+            "field": "region",
+            "type": "nominal",
+            "title": "Region"
+        },
+        "size": {
+            "field": "Ladder_score",
+            "type": "quantitative",
+            "scale": {
+                "type": "threshold",
+                "domain": [3, 4, 5, 6, 7],
+                "range": [40, 60, 80, 100, 150, 250]
+            },
+            "legend": {
+                "title": "Ladder Score"
+            }
         }
-    }
+    },
+    "transform": [
+        {
+            "lookup": "Country_name",
+            "from": {
+                "data": {
+                    "url": "https://raw.githubusercontent.com/dnylng/3179/main/Assignment2/data/continents.csv"
+                },
+                "key": "name",
+                "fields": ["region"]
+            }
+        },
+        {
+            "filter": {"field": "region", "oneOf": ["Africa", "Americas", "Asia", "Europe", "Oceania"]},
+        }
+    ]
 };
 
 let scatterSpecs = [
@@ -26,23 +60,35 @@ let scatterSpecs = [
     { "field": "Healthy_life_expectancy", "title": "Healthy Life Expectancy" },
     { "field": "GDP_per_capita", "title": "GDP Per Capita" },
     { "field": "Freedom_to_make_life_choices", "title": "Freedom to Make Life Choices" },
-    { "field": "Perceptions_of_corruption", "title": "Perceptions of Corruption" }
+    { "field": "Perceptions_of_corruption", "title": "Perceptions of Corruption" },
+    { "field": "Generosity", "title": "Generosity" }
 ];
 
 function updateScatterPlot(index) {
     const scatterSpec = JSON.parse(JSON.stringify(baseSpec));
+
     scatterSpec.encoding.y = {
         "field": scatterSpecs[index].field,
         "type": "quantitative",
         "title": scatterSpecs[index].title
     };
-    document.getElementById("scatterTitle").textContent = "Ladder Score vs. " + scatterSpecs[index].title;
-    vegaEmbed("#scatterPlot", scatterSpec, { mode: "vega-lite" }).then(console.log).catch(console.warn);
+
+    let filterValue = document.getElementById("happinessFilter").value;
+
+    scatterSpec.transform.push({
+        "filter": `datum.Ladder_score >= ${filterValue}`
+    });
+
+    document.getElementById("scatterTitle").textContent = "Ladder Score (>= " + filterValue + ") vs. " + scatterSpecs[index].title;
+    vegaEmbed("#scatterPlot", scatterSpec, { mode: "vega-lite", actions: false}).then(console.log).catch(console.warn);
 }
 
 document.getElementById("scatterSlider").addEventListener("input", function() {
-    updateScatterPlot(this.value - 1); // Adjusting since array indices start from 0
+    updateScatterPlot(this.value - 1);
 });
 
-// Initialize the first scatter plot
+document.getElementById("happinessFilter").addEventListener("input", function() {
+    updateScatterPlot(document.getElementById("scatterSlider").value - 1);
+});
+
 updateScatterPlot(0);
